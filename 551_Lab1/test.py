ecc0c663cf85b856c0ed58815aa1a342b6753fc8
apekii <56937246+apekii@users.noreply.github.com>
2022-02-08 14:32:18 -0700

    finished search results (+ looking into database for it)

--- 551_Lab1/test.py
+++ 551_Lab1/test.py
@@ -1,17 +1,13 @@
 import os
-
 import flask
-import flask_login
 import psycopg2
 
 from flask import Flask, render_template, request, url_for, session
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from werkzeug.security import check_password_hash
 from werkzeug.utils import redirect
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
-
 
 app = Flask(__name__)
 
@@ -29,10 +25,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://auruxcdprcbthb' \
                                         ':7ff31e2174704980e587ea5a91ef8b11a63b69ffe29b034a6b42a8c7318272e9@ec2-3-222' \
                                         '-127-58.compute-1.amazonaws.com:5432/da76or2kuol1r '
 
-# set session
+# set session secret key
 app.secret_key = 'ENGO551_Lab1'
 
-# set up database **MAKE SURE ITS postgresql AND NOT postgres**
+# set up database connection **MAKE SURE ITS postgresql AND NOT postgres**
 engine = create_engine('postgresql://auruxcdprcbthb:7ff31e2174704980e587ea5a91ef8b11a63b69ffe29b034a6b42a8c7318272e9'
                        '@ec2-3-222-127-58.compute-1.amazonaws.com:5432/da76or2kuol1r')
 db = scoped_session(sessionmaker(bind=engine))
@@ -45,26 +41,20 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-
 # already created users database
 # cur.execute("CREATE TABLE users(username varchar(255) NOT NULL PRIMARY KEY, password varchar("
 #            "255) NOT NULL);")
 
 
+# routes
 @app.route('/')
 def index():
     return render_template('index_book_search.html')
 
 
-@app.route('/book_search', methods=["GET", "POST"])
-def book_search():
-    return render_template('index_book_search.html')
-
-
 @app.route('/user_login', methods=["GET", "POST"])
-# make sure username and password are misspelled (there's nothing coded for that)
+# make sure username and password are not misspelled (there's nothing coded for that)
 def user_login():
-
     # clear session, in case someone has logged in already
     session.clear()
 
@@ -85,7 +75,7 @@ def user_login():
 
         # remember user
         session["user_name"] = result[0]
-        print(session)
+
         return render_template('user_profile.html')
     else:
         return render_template('user_login.html')
@@ -102,8 +92,7 @@ def user_profile():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        print(username)
-        print(password)
+
         cur.execute('INSERT INTO users (username,password) VALUES (%s, %s)',
                     (username, password))
 
@@ -119,6 +108,30 @@ def user_profile():
 @app.route('/user_register', methods=["GET"])
 def user_register():
     return render_template('user_register.html')
+
+
+@app.route('/book_search', methods=["GET"])
+def book_search():
+    if not request.args.get("book"):
+        return render_template("index_book_search.html")
+
+    # syntax of search title
+    # have '%%' to search in between titles
+    # capitalise titles
+    search = "'%" + request.args.get("book") + "%'"
+    search = search.title()
+
+    # look up item in database
+    sql_query = "SELECT * FROM books_csv WHERE title LIKE " + search + " OR isbn LIKE " + search + \
+                " OR isbn LIKE " + search + " OR author LIKE " + search + " OR author LIKE " + search
+    rows = db.execute(sql_query)
+    books = rows.fetchall()
+
+    # if no results found
+    if rows.rowcount == 0:
+        return render_template('book_results.html', message="No results found", search_name=request.args.get("book"))
+
+    return render_template('book_results.html', books=books, search_name=request.args.get("book"))
 
 
 if __name__ == '__main__':
