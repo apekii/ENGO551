237883dc1aab8caa7e5266f66e951fa61b20bb4d
apekii <56937246+apekii@users.noreply.github.com>
2022-02-16 18:32:03 -0700

    Starting to connect JS file with HTML

--- 551_Lab2/app.py
+++ 551_Lab2/app.py
@@ -1,17 +1,48 @@
 import os
+import requests
+import pandas as pd
 
-from flask import Flask, flash, render_template, request, url_for, session
+from sodapy import Socrata
+from datetime import datetime
+from flask import Flask, flash, render_template, session, request, url_for
 
-def map_details(date):
-    # api for Google Books ratings
-    calg_bp = requests.get("https://data.calgary.ca/resource/c2es-76ed.json")
+app = Flask(__name__)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+# from https://dev.socrata.com/foundry/data.calgary.ca/c2es-76ed
+client = Socrata("data.calgary.ca", None)
+results = client.get("c2es-76ed", limit=1)
+results_df = pd.DataFrame.from_records(results)
+
+
+# routes
+@app.route('/')
+def index():
+    return render_template("map.html", features='')
+
+
+@app.route('/search', methods=["GET", "POST"])
+def map_details():
+    # api for Calgary Building Permits
+    from_date = "'" + datetime.strptime(str(request.form.get("from_date")),'%Y-%m-%d').strftime('%Y/%m/%d') + "'"
+    to_date = "'" + datetime.strptime(str(request.form.get("to_date")),'%Y-%m-%d').strftime('%Y/%m/%d') + "'"
+    from_date = "'" + request.form.get("from_date") + "T00:00:00.000" + "'"
+    to_date = "'" + request.form.get("to_date") + "T23:59:00.000" + "'"
+    url = "https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate between " + from_date + " and " + to_date + "&$select=issueddate,latitude,longitude,workclassgroup,contractorname,communityname,originaladdress"
+
+    calg_bp = requests.get(url)
     response = calg_bp.json()
+    features = response['features']
 
-    results =
-    rating = (response['items'][0]['volumeInfo']['averageRating'])
-    no_rating = (response['items'][0]['volumeInfo']['ratingsCount'])
+    print(url)
+    #print(features)
 
-    return render_template("map.html", results=results)
+    return render_template("map.html", response=response)
 
 # how do you render_template when you're just refreshing
 # the map and not going to different html page?
