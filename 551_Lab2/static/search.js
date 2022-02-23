082dde57eac8a94b77e8992e7273e8b004b6ee2e
apekii <56937246+apekii@users.noreply.github.com>
2022-02-22 22:37:17 -0700

    Added Mapbox layer Changed cluster colour to be more visible on layer Added checkbox to view traffic incidents

--- 551_Lab2/static/search.js
+++ 551_Lab2/static/search.js
@@ -1,3 +1,30 @@
-function BP_locations(){
-    document.write("IN JS FILE")
+function BP_locations(geojson_features){
+
+    var popup = new L.Popup();
+
+    var oms = new OverlappingMarkerSpiderfier(mymap);
+    oms.addListener('click', function(marker) {
+    popup.setContent(marker.desc);
+    popup.setLatLng(marker.getLatLng());
+    mymap.openPopup(popup);
+    });
+    oms.addListener('spiderfy', function(markers) {
+        mymap.closePopup();
+    });
+
+    var markers = L.markerClusterGroup();
+
+    for (var i=0; i<Object.keys(geojson_features).length;i++){
+        var loc = new L.LatLng(geojson_features[i].properties.latitude, geojson_features[i].properties.longitude);
+        var marker = new L.Marker(loc);
+        var popcontent="Issued Date: "+geojson_features[i].properties.issueddate+"<br>"+
+                        "Work Class Group: "+geojson_features[i].workclassgroup+"<br>"+
+                        "Contractor Name: "+geojson_features[i].contractorname+"<br>"+
+                        "Community Name: "+geojson_features[i].communityname+"<br>"+
+                        "Original Address: "+geojson_features[i].originaladdress;
+        marker.desc = popcontent;
+        mymap.addLayer(markers);
+        oms.addMarker(marker);
+        markers.addLayer(marker);
+    }
 }
